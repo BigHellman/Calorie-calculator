b41187c7bd133c753ca8d9effefda6f30130c2be
BigHellman <95682636+BigHellman@users.noreply.github.com>
2022-02-27 18:32:43 +0100

    Update CalorieCalculator.py

--- CalorieCalculator.py
+++ CalorieCalculator.py
@@ -1,26 +1,69 @@
-from kivy import kivy_register_post_configuration
-
-
-def user_info():
+  def user_info():
     kön = input('Vad är ditt kön?: ')
     ålder = int(input('Vad är din ålder?: '))
     längd = int(input('Hur lång är du i centimeter?: '))
     vikt = int(input('Hur mycket väger du?: '))
 
-    if kön == 'man':
+    if kön.lower() == 'man':
         c1 = 66
-        hm = 6.2 * längd
-        wm = 12.7 * vikt
-        am = 6.76 * ålder
-    elif kön == 'kvinna':
-        c1 = 655.1 
-        hm = 4.35 * längd
-        wm = 4.7 * vikt
+        hm = 5.0 * längd
+        wm = 13.7 * vikt
+        am = 6.8 * ålder
+    elif kön.lower() == 'kvinna':
+        c1 = 655 
+        hm = 1.8 * längd
+        wm = 9.6 * vikt
         am = 4.7 * ålder
 
-    #bmr = 665 + (9.6 X 69) + (1.8 X 178) - (4.7 X 27)
+    #bmr = 66 + (13,7 * vikt) + (5 * längd) - (6,8 * ålder)
     bmr_resultat = c1 + hm + wm - am
     return(int(bmr_resultat))
 
 def kalkylera_träning(bmr_resultat):
+    tränings_nivå = input('Hur många gånger tränar du i veckan? (0, 1-2, 3-4, 5-6, 7 ): ')
     
+    if tränings_nivå == '0':
+        tränings_nivå = 1.2 * bmr_resultat
+    elif tränings_nivå == '1-2':
+        tränings_nivå = 1.375 * bmr_resultat
+    elif tränings_nivå == '3-4':
+        tränings_nivå = 1.55 * bmr_resultat
+    elif tränings_nivå == '5-6':
+        tränings_nivå = 1.725 * bmr_resultat
+    elif tränings_nivå == '7':
+        tränings_nivå = 1.9 * bmr_resultat
+    
+    return(int(tränings_nivå))
+
+def gå_upp_eller_gå_ner(tränings_nivå):
+    mål = input('Vill du gå upp, ner eller hålla i vikt?: ')
+
+    if mål.lower() == 'gå ner':
+        kalorier = tränings_nivå - 500
+    elif mål.lower() == 'hålla':
+        kalorier = tränings_nivå
+    elif mål.lower() == 'gå upp':
+        gå_upp = int(input('Gå upp 0,5 eller 1 kilo i veckan? Skriv 0.5 eller 1: '))
+        if gå_upp == 0.5:
+            kalorier = tränings_nivå + 500
+        elif gå_upp == 1:
+            kalorier = tränings_nivå + 1000
+
+    print('för att kunna ', mål, 'vikt, ditt dagliga kalori intag borde vara', int(kalorier), '!')
+    
+gå_upp_eller_gå_ner(kalkylera_träning(user_info()))
+
+
+def trängins_pass(tränings_pass):
+    träningspass = input('Vad vill du träna idag? : Välj mellan rygg, bröst, ben, armar och axlar, rygg och bröst: ')
+
+    if träningspass.lower() == 'rygg':
+        print('Stående rod, T-bar rod, Lats, En arms hantel rod, Rep pullover: ')
+    elif träningspass.lower() == 'bröst':
+        print('Bänkpress, Snebänk med hantlar, Kabel flys, Hantel flys, peckdeck: ')
+    elif träningspass.lower() == 'ben':
+        print('Benböj, RDLs, benpress, vader: ')
+    elif träningspass.lower() == 'armar och axlar':
+        print('axelpress, ')
+
+    return(int(tränings_pass))
